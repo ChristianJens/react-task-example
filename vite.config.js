98f02e5a212e0a0abf23250c5469f3dfb26ca0d4
Christian Jens Landio Cruz <vagoiluminado12@hotmail.com>
2023-10-24 05:29:43 -0500

    add: files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Poniedo la configuracion para despliegue
-  base: '/https://github.com/ChristianJens/react-tasks-deploy/',
   plugins: [react()],
-})
+  // Poniedo la configuracion para despliegue
+  // base: '/https://github.com/ChristianJens/react-tasks-deploy/',
+  base: "/react-task-example/",
+});
