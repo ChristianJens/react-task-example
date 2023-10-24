5828de9cf4280c66b0cf9e9396f766d011056707
Christian Jens Landio Cruz <vagoiluminado12@hotmail.com>
2023-10-23 21:13:19 -0500

    Five Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   // Poniedo la configuracion para despliegue
-  base: '/https://github.com/ChristianJens/react-deploy-example/'
+  base: '/https://github.com/ChristianJens/react-deploy-example/',
+  plugins: [react()],
 })
